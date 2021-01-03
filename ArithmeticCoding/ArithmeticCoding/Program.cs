f886b8527980046040e9504a6f57abb15180f78e
Anastasiya Danilenka <41057639+adanilenka@users.noreply.github.com>
2021-01-03 19:37:30 +0300

    ArithmeticCodingStart

---
+++ ArithmeticCoding/ArithmeticCoding/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArithmeticCoding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.ReadAllText(@"C:\Users\Nasyat\source\repos\ArithmeticCoding\ArithmeticCoding\Test.txt", Encoding.Default);
+        }
+
+
+    }
+}
