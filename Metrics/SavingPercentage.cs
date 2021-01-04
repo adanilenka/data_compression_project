a75f76723e3f76237c43c64308df71552e9e4577
adanilenka <asti4asya@gmail.com>
2021-01-04 15:00:35 +0300

    Change folder

---
+++ Metrics/SavingPercentage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Metrics
+{
+    public static class SavingPercentage
+    {
+        public static float Calculate(string sourcefileName, string codedfileName)
+        {
+            long sourceFileLength = new FileInfo(sourcefileName).Length;
+            long codedFileLength = new FileInfo(codedfileName).Length;
+            return (((float)sourceFileLength - (float)codedFileLength) / (float)sourceFileLength) * 100;
+        }
+    }
+}
